e4e1832061ae7b17ae1d36770686b8cd2b13778c
Kamil Krzysztof Lipski <kamil.k.lipski@gmail.com>
2018-12-28 13:17:51 +0100

    wersja 1.0 - szkielety plików nagłówkowych i źródłowych

---
+++ aal/algorithm.cpp
@@ -0,0 +1,9 @@
+/**
+* Name: Algorytm
+* Purpose: Problem plecakowy
+*
+* @author Kamil Krzysztof Lipski
+* @version 1.0
+*/
+
+#include "pch.h"
---
+++ aal/pch.h
@@ -0,0 +1,7 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "algorithm.h"
+#include "interface.h"
+
+#endif //PCH_H
